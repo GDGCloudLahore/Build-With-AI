254f6d482588d4eb3f29d10b2b6627163cb74b05
Adil Shehzad <adil.shehzad4146@gmail.com>
2024-04-12 20:21:29 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Google-IO-Extended-2024/',
+  base: '/Build-With-AI/',
   plugins: [react()],
 })
